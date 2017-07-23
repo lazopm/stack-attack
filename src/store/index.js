0f2ba1740325373994af69025d9b76fcc1af1114
Pablo Lazo <lazopm@gmail.com>
2017-07-23 17:56:53 -0400

    Add redux-thunk middleware

--- src/store/index.js
+++ src/store/index.js
@@ -1,6 +1,13 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk'
+
 import reducers from './reducers';
 
-const create = (state = {}) => createStore(reducers, state);
+const create = (initialState = {}) =>
+    createStore(
+        reducers,
+        applyMiddleware(thunk),
+        initialState,
+    );
 
 export { create as createStore };
