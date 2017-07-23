aabed24423e1772b05c1a00f4f99a3ec4386d32b
Pablo Lazo <lazopm@gmail.com>
2017-07-23 19:24:19 -0400

    Add busy reducer

---
+++ src/store/reducers/busy.js
@@ -0,0 +1,18 @@
+import {
+    FETCH_QUESTION,
+    FETCH_QUESTION_SUCCESS,
+    FETCH_QUESTION_ERROR,
+} from 'store/actions'; 
+
+export default (state = false, { type, ...action }) => {
+    switch (type) {
+        case FETCH_QUESTION:
+            return true;
+        case FETCH_QUESTION_SUCCESS:
+            return false;
+        case FETCH_QUESTION_ERROR:
+            return false;
+        default:
+            return state;
+    }
+};
--- src/store/reducers/index.js
+++ src/store/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux'
 import question from './question'
 import answers from './answers'
+import busy from './answers'
 
 export default combineReducers({
     question,
     answers,
+    busy,
 });
