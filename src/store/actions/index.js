6fe4ea386be9fd535a3d6c65a3a4381a33009e27
Pablo Lazo <lazopm@gmail.com>
2017-07-23 20:28:39 -0400

    WIP new question actions

--- src/store/actions/index.js
+++ src/store/actions/index.js
@@ -1,6 +1,26 @@
-export const NEW_QUESTION = 'NEW_QUESTION';
-export const newQuestion = (question, answers) => ({
-    type: NEW_QUESTION,
-    question,
-    answers,
-});
+import { getQuestion } from 'api/stackoverflow';
+
+export const FETCH_QUESTION = 'FETCH_QUESTION';
+export const FETCH_QUESTION_SUCCESS = 'FETCH_QUESTION_SUCCESS';
+export const FETCH_QUESTION_ERROR = 'FETCH_QUESTION_ERROR';
+
+export const newQuestion = () =>
+    async (dispatch, getState) => {
+        dispatch({
+            type: FETCH_QUESTION,
+        });
+        try {
+            const { question, answers } = await getQuestion();
+            dispatch({
+                type: FETCH_QUESTION_SUCCESS,
+                question,
+                answers,
+            });
+        }
+        catch(error) {
+            dispatch({
+                type: FETCH_QUESTION_ERROR,
+                error,
+            });
+        }
+    };
--- src/store/reducers/question.js
+++ src/store/reducers/question.js
@@ -1,7 +1,17 @@
+import {
+    FETCH_QUESTION,
+    FETCH_QUESTION_SUCCESS,
+    FETCH_QUESTION_ERROR,
+} from 'store/actions'; 
+
 export default (state = null, { type, ...action }) => {
     switch (type) {
-        case 'NEW_QUESTION':
+        case FETCH_QUESTION:
+            return null;
+        case FETCH_QUESTION_SUCCESS:
             return action.question;
+        case FETCH_QUESTION_ERROR:
+            return null;
         default:
             return state;
     }
