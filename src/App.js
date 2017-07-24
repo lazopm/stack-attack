43f262c9c02c913b2ebf321e0abf0dd2350394d0
Pablo Lazo <lazopm@gmail.com>
2017-07-23 23:51:22 -0400

    Create Answer and Question components

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 import { newQuestion } from 'store/actions';
+import Answer from 'components/Answer';
+import Question from 'components/Question';
 
 class App extends Component {
     componentWillMount() {
         this.props.newQuestion();
     }
     render() {
+        const { question, answers } = this.props;
         return (
             <div>
-                hello world
+                <Question {...question} />
+                {answers.map(({ id, ...props }) =>
+                    <Answer key={id} {...props} />
+                )}
             </div>
         );
     }
@@ -21,9 +27,11 @@ const mapDispatchToProps = {
 };
 
 const matchStateToProps = ({
+    busy,
     question,
     answers
 }) => ({
+    busy,
     question,
     answers,
 });
---
+++ src/components/Question/index.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const Question = () => <div>QUESTION</div>
+
+export default Question;
