7844f7f96a59a9ebb2febd0ed6cbb50ca184dcf0
Pablo Lazo <lazopm@gmail.com>
2017-07-23 21:30:54 -0400

    Add randomItem util function

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,3 +1,9 @@
 export const queryString = params => Object.entries(params)
     .map(([name, value]) => `${name}=${value}`)
     .join('&');
+
+//gets a random item from an array
+export const randomItem = arr => {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    return arr[randomIndex];
+};
